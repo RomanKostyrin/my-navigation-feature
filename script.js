2589067233257a4f9fcc623e5f85691afadbad5d
roman kostyrin <enemy-iubip@mail.ru>
2021-07-11 12:38:53 +0300

    refactoring nv code and made opened subPosts correct

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
 const button = document.querySelector('.button')
 const containers = document.querySelectorAll('.container')
-
 const ulAll = document.querySelectorAll('ul')
 const list = document.querySelector('.list')
 const buttons = document.querySelectorAll('.button')
 
+let myButton = 0
 let mainObject = {
   activeFirstMenu: 'main-0',
   menus: [2, 1, 1],
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const id = +getIndexFromSome(mainObject.activeFirstMenu)
+    let mainElement = document.getElementById(mainObject.activeFirstMenu)
+    myButton = button
+    event.stopPropagation()
+    if (button.parentNode.parentNode.classList.contains('list')) {
+      cleareActiveClasses(mainObject.activeFirstMenu)
+      button.parentNode.classList.add('containerActive')
+      mainObject.activeFirstMenu = button.parentNode.id
+      mainObject.menus[+getIndexFromSome(mainObject.activeFirstMenu)] = 2
+    } else {
+      myButton.classList.add('buttonSecond')
+      mainObject.menus[+getIndexFromSome(mainObject.activeFirstMenu)] = 3
+      button.parentNode.parentNode.classList.add('visuallyHidden')
+      button.parentNode.parentNode.parentNode.classList.add('containerChoosen')
+      button.parentNode.classList.add('containerActive')
+      mainElement.append(myButton)
+    }
+  })
+})
+
+function cleareActiveClasses(id) {
+  let index = getIndexFromSome(id)
+  mainObject.menus[index] = 1
+  document.getElementById(id).classList.remove('containerActive')
 }
 
 let getIndexFromSome = (string) => {
   const indexOfDash = string.indexOf('-')
   const newIndex = string.slice(indexOfDash + 1, string.length)
   return newIndex
-}
-
-let myButton = 0
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const id = +mainObject.activeFirstMenu
-    let mainElement = document.getElementById(`main-${id}`)
-    myButton = button
-
-    event.stopPropagation()
-    if (button.parentNode.parentNode.classList.contains('list')) {
-      console.log('before ' + mainObject.activeFirstMenu)
-      cleareActiveClasses(mainObject.activeFirstMenu)
-      mainObject.activeFirstMenu = button.parentNode.id
-      button.parentNode.classList.add('containerActive')
-      console.log('after ' + mainObject.activeFirstMenu)
-    } else {
-      button.parentNode.parentNode.classList.add('visuallyHidden')
-      button.parentNode.classList.add('containerActive')
-      mainElement.append(myButton)
-    }
-
-    showSubMenu(button.parentNode)
-  })
-})
-
-function cleareActiveClasses(id) {
-  console.log(document.getElementById(id))
-  document.getElementById(id).classList.remove('containerActive')
-}
-
-function showSubMenu(container) {
-  container.children[1].classList.add('subListActive')
 }
